f25b426f4feaf9dc3ee8a147b487c596fc7fa4b2
Torsten Werner <torsten.werner@westernacher.com>
2019-01-16 20:40:29 +0100

    initial import

---
+++ src/main/java/com/example/textanalysis/TextanalysisApplication.java
@@ -0,0 +1,23 @@
+package com.example.textanalysis;
+
+import org.apache.commons.text.similarity.CosineDistance;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextanalysisApplication implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextanalysisApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        final CosineDistance cosineDistance = new CosineDistance();
+        final Double distance = cosineDistance.apply("Hallo schöne Welt!", "Tschüss Welt!");
+        System.out.printf("cosine distance: %f%n", distance);
+    }
+}
+
